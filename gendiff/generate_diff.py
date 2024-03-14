d8743a26a5e27ab87c6bc138e41031bbf2580e95
Lukashhhhh <131288459+Lukashhhhh@users.noreply.github.com>
2024-03-14 12:54:20 +0000

    Refactoring generate_diff.py
    The choice of format is made in formatters\format_selection.py

--- gendiff/generate_diff.py
+++ gendiff/generate_diff.py
@@ -1,19 +1,11 @@
 from gendiff.read_file import get_file_data
-from gendiff.parser import get_sorted_diff
-from gendiff.format.json import get_format_json
-from gendiff.format.stylish import get_format_stylish
-from gendiff.format.plain import get_format_plain
+from gendiff.diff_tree import build_diff_tree
+from gendiff.formatters.format_selection import get_formated_data
 
 
 def generate_diff(file_path1, file_path2, format_name='stylish'):
     data_file1 = get_file_data(file_path1)
     data_file2 = get_file_data(file_path2)
-    sorted_diff = get_sorted_diff(data_file1, data_file2)
-    match format_name:
-        case 'stylish':
-            formated_data = get_format_stylish(sorted_diff)
-        case 'plain':
-            formated_data = get_format_plain(sorted_diff)
-        case 'json':
-            formated_data = get_format_json(sorted_diff)
+    sorted_diff = build_diff_tree(data_file1, data_file2)
+    formated_data = get_formated_data(format_name, sorted_diff)
     return formated_data
