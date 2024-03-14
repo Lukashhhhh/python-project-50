3aabc816aa2bb76a76eae17c422f49d4b22e05b6
Lukashhhhh <131288459+Lukashhhhh@users.noreply.github.com>
2024-03-14 12:54:54 +0000

    Refactoring test_generate_diff.py

--- test/test_generate_diff.py
+++ test/test_generate_diff.py
@@ -6,21 +6,13 @@ import pytest
     ('test/fixtures/file1.json', 'test/fixtures/file2.json'),
     ('test/fixtures/file1.yml', 'test/fixtures/file2.yml'),
 ])
-def test_generate_diff_stylish(file_path1, file_path2):
+def test_generate_diff_stylish_plain(file_path1, file_path2):
     result_stylish_path = 'test/fixtures/result_file_stylish.txt'
-    with open(result_stylish_path) as stylish_result:
-        assert generate_diff(file_path1, file_path2) == stylish_result.read()
-
-
-@pytest.mark.parametrize("file_path1, file_path2", [
-    ('test/fixtures/file1.json', 'test/fixtures/file2.json'),
-    ('test/fixtures/file1.yml', 'test/fixtures/file2.yml'),
-])
-def test_generate_diff_plain(file_path1, file_path2):
     result_plain_path = 'test/fixtures/result_file_plain.txt'
-    with open(result_plain_path) as plain_result:
-        assert generate_diff(file_path1, file_path2,
-                             format_name='plain') == plain_result.read()
+    with open(result_stylish_path) as stylish_result, open(
+            result_plain_path) as plain_result:
+        assert generate_diff(file_path1, file_path2) == stylish_result.read()
+        assert generate_diff(file_path1, file_path2, 'plain') == plain_result.read()
 
 
 @pytest.mark.parametrize("file_path1, file_path2", [
