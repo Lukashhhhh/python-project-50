0d0ef8697984123889052a22db6ed57b06b319e2
Lukashhhhh <131288459+Lukashhhhh@users.noreply.github.com>
2024-03-15 07:53:55 +0000

    Refactoring plain.py

--- gendiff/formatters/plain.py
+++ gendiff/formatters/plain.py
@@ -1,17 +1,13 @@
-def get_correct_format(value):
-    format_ = {
-        bool: str(value).lower(),
-        type(None): 'null',
-        str: f"'{value}'"
-    }
+def transform_to_str(value):
+    format_ = {bool: str(value).lower(), type(None): 'null', str: f"'{value}'"}
     if type(value) in format_:
         return format_[type(value)]
-    return value
+    return str(value)
 
 
 def get_format_plain(current_value, path=[]):
     if not isinstance(current_value, dict):
-        return get_correct_format(current_value)
+        return transform_to_str(current_value)
     result = []
     for key, value in current_value.items():
         if isinstance(current_value, dict) and 'status' in value:
@@ -20,25 +16,18 @@ def get_format_plain(current_value, path=[]):
             match value['status']:
                 case 'added':
                     children = value['children']
-                    result.append(
-                        f"Property '{current_path}' was added with "
-                        f"value: {get_format_plain(children, path)}"
-                    )
+                    result.append(f"Property '{current_path}' was added with "
+                                  f"value: {get_format_plain(children, path)}")
                 case 'deleted':
                     result.append(f"Property '{current_path}' was removed")
                 case 'changed':
                     child1, child2 = value['child1'], value['child2']
-                    result.append(
-                        f"Property '{current_path}' was updated. "
-                        f"From {get_format_plain(child1)} to "
-                        f"{get_format_plain(child2)}"
-                    )
+                    result.append(f"Property '{current_path}' was updated. "
+                                  f"From {get_format_plain(child1)} to "
+                                  f"{get_format_plain(child2)}")
                 case 'nested':
                     children = value['children']
                     result.append(get_format_plain(children, path))
-                case 'unchanged':
-                    path.pop()
-                    continue
             path.pop()
         else:
             return '[complex value]'
