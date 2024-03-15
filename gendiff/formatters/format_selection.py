f0b3d5ec2895a3d6bea5b22fb1568fe8f3b1e93d
Lukashhhhh <131288459+Lukashhhhh@users.noreply.github.com>
2024-03-15 07:28:16 +0000

    Refactoring format_selection.py

--- gendiff/formatters/format_selection.py
+++ gendiff/formatters/format_selection.py
@@ -2,6 +2,7 @@ from gendiff.formatters.json import get_format_json
 from gendiff.formatters.stylish import get_format_stylish
 from gendiff.formatters.plain import get_format_plain
 
+
 def get_formated_data(format, data):
     match format:
         case 'stylish':
