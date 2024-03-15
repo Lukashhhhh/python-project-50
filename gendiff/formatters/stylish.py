d96820e63b540f2ec4f71fc680efe4b1de224850
Lukashhhhh <131288459+Lukashhhhh@users.noreply.github.com>
2024-03-15 07:29:17 +0000

    Refactoring stylish.py

--- gendiff/formatters/stylish.py
+++ gendiff/formatters/stylish.py
@@ -5,10 +5,7 @@ SIGN = {'added': '+', 'deleted': '-', 'unchanged': ' '}
 
 
 def transform_to_str(value):
-    format_ = {
-        bool: str(value).lower(),
-        type(None): 'null'
-    }
+    format_ = {bool: str(value).lower(), type(None): 'null'}
     if type(value) in format_:
         return format_[type(value)]
     return str(value)
@@ -16,33 +13,32 @@ def transform_to_str(value):
 
 def get_format_stylish(current_value, depth=CURRENT_DEPTH):
     if not isinstance(current_value, dict):
-        # current_value = get_formated_data(current_value)
         return f'{transform_to_str(current_value)}'
-    result = ['{']
+    result = []
     indent = depth * NUMBER_OF_INDENTS + '}'
     gen_indent = depth * NUMBER_OF_INDENTS + OFFSET_TO_THE_LEFT
     for key, value in current_value.items():
         if isinstance(value, dict) and 'status' in value:
             match value['status']:
                 case 'nested' | 'unchanged':
-                    result.append(f'\n{gen_indent}{SIGN["unchanged"]} {key}: ')
+                    children = value['children']
+                    result.append(f'{gen_indent}{SIGN["unchanged"]} {key}: '
+                                  f'{get_format_stylish(children, depth + 1)}')
                 case 'added':
-                    result.append(f'\n{gen_indent}{SIGN["added"]} {key}: ')
+                    children = value['children']
+                    result.append(f'{gen_indent}{SIGN["added"]} {key}: '
+                                  f'{get_format_stylish(children, depth + 1)}')
                 case 'deleted':
-                    result.append(f'\n{gen_indent}{SIGN["deleted"]} {key}: ')
+                    children = value['children']
+                    result.append(f'{gen_indent}{SIGN["deleted"]} {key}: '
+                                  f'{get_format_stylish(children, depth + 1)}')
                 case 'changed':
-
                     child1, child2 = value["child1"], value["child2"]
-                    result.append(
-                        f'\n{gen_indent}{SIGN["deleted"]} {key}: '
-                        f'{get_format_stylish(child1, depth + 1)}\n'
-                        f'{gen_indent}{SIGN["added"]} {key}: '
-                        f'{get_format_stylish(child2, depth + 1)}'
-                    )
-                    continue
-            children = value['children']
-            result.append(get_format_stylish(children, depth + 1))
+                    result.append(f'{gen_indent}{SIGN["deleted"]} {key}: '
+                                  f'{get_format_stylish(child1, depth + 1)}\n'
+                                  f'{gen_indent}{SIGN["added"]} {key}: '
+                                  f'{get_format_stylish(child2, depth + 1)}')
         else:
-            result.append(f'\n{gen_indent}{SIGN["unchanged"]} {key}: '
-                       f'{get_format_stylish(value, depth + 1)}')
-    return f'\n{indent}'.join(result)
+            result.append(f'{gen_indent}{SIGN["unchanged"]} {key}: '
+                          f'{get_format_stylish(value, depth + 1)}')
+    return '{\n' + '\n'.join(result) + f'\n{indent}'
