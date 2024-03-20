a5f7a92950c3e5be0792c8f256e3439734d59353
Lukashhhhh <131288459+Lukashhhhh@users.noreply.github.com>
2024-03-20 20:33:21 +0000

    Refactoring test_generate_diff.py

--- test/test_generate_diff.py
+++ test/test_generate_diff.py
@@ -2,28 +2,27 @@ from gendiff import generate_diff
 import pytest
 
 
-@pytest.mark.parametrize("file_path1, file_path2, format_name", [
-    ('test/fixtures/file1.json', 'test/fixtures/file2.json', 'stylish'),
-    ('test/fixtures/file1.yml', 'test/fixtures/file2.yml', 'stylish'),
-    ('test/fixtures/file1.json', 'test/fixtures/file2.json', 'plain'),
-    ('test/fixtures/file1.yml', 'test/fixtures/file2.yml', 'plain'),
+@pytest.mark.parametrize("file_path1, file_path2", [
+    ('test/fixtures/file1.json', 'test/fixtures/file2.json'),
+    ('test/fixtures/file1.yml', 'test/fixtures/file2.yml'),
 ])
+@pytest.mark.parametrize("format_name", ('stylish', 'plain', 'json', 'txt'))
 def test_generate_diff_stylish_plain(file_path1, file_path2, format_name):
     result_stylish_path = 'test/fixtures/result_file_stylish.txt'
     result_plain_path = 'test/fixtures/result_file_plain.txt'
     match format_name:
         case 'stylish':
             with open(result_stylish_path) as stylish_result:
-                result = stylish_result.read()
+                assert generate_diff(file_path1, file_path2,
+                                     format_name) == stylish_result.read()
         case 'plain':
             with open(result_plain_path) as plain_result:
-                result = plain_result.read()
-    assert generate_diff(file_path1, file_path2, format_name) == result
-
-
-@pytest.mark.parametrize("file_path1, file_path2", [
-    ('test/fixtures/file1.json', 'test/fixtures/file2.json'),
-    ('test/fixtures/file1.yml', 'test/fixtures/file2.yml'),
-])
-def test_generate_diff_json(file_path1, file_path2):
-    assert isinstance(generate_diff(file_path1, file_path2, 'json'), str)
+                assert generate_diff(file_path1, file_path2,
+                                     format_name) == plain_result.read()
+        case 'json':
+            assert isinstance(
+                generate_diff(file_path1, file_path2, format_name), str)
+        case _:
+            with open(result_stylish_path) as stylish_result:
+                assert generate_diff(file_path1, file_path2,
+                                     format_name) == stylish_result.read()
