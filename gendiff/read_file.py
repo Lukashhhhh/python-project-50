ece3db4826fc2a741090c13ddc8c3a4243d37068
Lukashhhhh <131288459+Lukashhhhh@users.noreply.github.com>
2024-03-20 20:33:37 +0000

    Refactoring read_file.py

--- gendiff/read_file.py
+++ gendiff/read_file.py
@@ -4,13 +4,14 @@ import yaml
 
 
 def get_file_data(file_path: str):
-    _, expansion = os.path.splitext(file_path)
-    return extract_data_from_file(file_path, expansion)
+    _, extension = os.path.splitext(file_path)
+    with open(file_path) as file:
+        return extract_data_from_file(file, extension)
 
 
-def extract_data_from_file(file_path: str, expansion: str):
-    match expansion:
+def extract_data_from_file(file, extension: str):
+    match extension:
         case '.json':
-            return json.load(open(file_path))
+            return json.load(file)
         case '.yml' | '.yaml':
-            return yaml.safe_load(open(file_path))
+            return yaml.safe_load(file)
