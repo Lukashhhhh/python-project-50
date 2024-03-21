07573c08ac9539dd1cf8fac98b3c779b1ddd26c0
ivan <iv1uk@yandex.ru>
2024-03-22 01:27:46 +0300

    refactoring diff_tree.py

--- gendiff/diff_tree.py
+++ gendiff/diff_tree.py
@@ -5,15 +5,15 @@ def build_diff_tree(data1, data2):
         if key not in data1:
             result[key] = {
                 'status': 'added',
-                'children': data2[key]}
+                'value': data2[key]}
         elif key not in data2:
             result[key] = {
                 'status': 'deleted',
-                'children': data1[key]}
+                'value': data1[key]}
         elif data1[key] == data2[key]:
             result[key] = {
                 'status': 'unchanged',
-                'children': data1[key]}
+                'value': data1[key]}
         elif isinstance(data1[key], dict) and isinstance(data2[key], dict):
             result[key] = {
                 'status': 'nested',
@@ -21,8 +21,8 @@ def build_diff_tree(data1, data2):
         else:
             result[key] = {
                 'status': 'changed',
-                'child1': data1[key],
-                'child2': data2[key]
+                'old_value': data1[key],
+                'new_value': data2[key]
             }
     sorted_diff = dict(sorted(result.items()))
     return sorted_diff
