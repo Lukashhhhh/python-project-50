b425a1eae201795ca2a6bf32bac1eaf23e4785f7
ivan <iv1uk@yandex.ru>
2024-03-22 01:27:10 +0300

    refactoring generate_diff.py

--- gendiff/generate_diff.py
+++ gendiff/generate_diff.py
@@ -3,9 +3,9 @@ from gendiff.diff_tree import build_diff_tree
 from gendiff.formatters.format_selection import get_formated_data
 
 
-def generate_diff(file_path1, file_path2, format_name='stylish'):
+def generate_diff(file_path1, file_path2, format='stylish'):
     data_file1 = get_file_data(file_path1)
     data_file2 = get_file_data(file_path2)
     sorted_diff = build_diff_tree(data_file1, data_file2)
-    formated_data = get_formated_data(format_name, sorted_diff)
+    formated_data = get_formated_data(format, sorted_diff)
     return formated_data
