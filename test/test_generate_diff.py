0589f031c8da328c24b5c6cc161e86fb47714075
ivan <iv1uk@yandex.ru>
2024-03-22 01:23:26 +0300

    Refactoring test_generate_diff.py

--- test/test_generate_diff.py
+++ test/test_generate_diff.py
@@ -1,28 +1,17 @@
 from gendiff import generate_diff
 import pytest
 
-
 @pytest.mark.parametrize("file_path1, file_path2", [
     ('test/fixtures/file1.json', 'test/fixtures/file2.json'),
     ('test/fixtures/file1.yml', 'test/fixtures/file2.yml'),
+    pytest.param('test/fixtures/file1.txt', 'test/fixtures/file2.txt', marks=pytest.mark.xfail)
 ])
-@pytest.mark.parametrize("format_name", ('stylish', 'plain', 'json', 'txt'))
-def test_generate_diff_stylish_plain(file_path1, file_path2, format_name):
-    result_stylish_path = 'test/fixtures/result_file_stylish.txt'
-    result_plain_path = 'test/fixtures/result_file_plain.txt'
-    match format_name:
-        case 'stylish':
-            with open(result_stylish_path) as stylish_result:
-                assert generate_diff(file_path1, file_path2,
-                                     format_name) == stylish_result.read()
-        case 'plain':
-            with open(result_plain_path) as plain_result:
-                assert generate_diff(file_path1, file_path2,
-                                     format_name) == plain_result.read()
-        case 'json':
-            assert isinstance(
-                generate_diff(file_path1, file_path2, format_name), str)
-        case _:
-            with open(result_stylish_path) as stylish_result:
-                assert generate_diff(file_path1, file_path2,
-                                     format_name) == stylish_result.read()
+@pytest.mark.parametrize("format, result_path", [
+    ('stylish', 'test/fixtures/result_file_stylish.txt'),
+    ('plain', 'test/fixtures/result_file_plain.txt'),
+    ('json', 'test/fixtures/result_file_json.txt'),
+    pytest.param('txt', 'test/fixtures/result_file_stylish.txt', marks=pytest.mark.xfail)
+])
+def test_generate_diff_stylish_plain(file_path1, file_path2, format, result_path):
+    with open(result_path) as result:
+        assert generate_diff(file_path1, file_path2, format) == result.read()
