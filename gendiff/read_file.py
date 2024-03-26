1917c4b7995c64d4e30e9f39fa45a766d3639f22
Lukashhhhh <131288459+Lukashhhhh@users.noreply.github.com>
2024-03-26 10:03:12 +0000

    Refactoring read_file.py

--- gendiff/read_file.py
+++ gendiff/read_file.py
@@ -5,15 +5,15 @@ import yaml
 
 def get_file_data(file_path: str):
     _, extension = os.path.splitext(file_path)
-    with open(file_path) as file:
-        return extract_data_from_file(file, extension[1:])
+    with open(file_path) as content:
+        return parse(content, extension[1:])
 
 
-def extract_data_from_file(file, extension: str):
-    match extension:
+def parse(content, format: str):
+    match format:
         case 'json':
-            return json.load(file)
+            return json.load(content)
         case 'yml' | 'yaml':
-            return yaml.safe_load(file)
+            return yaml.safe_load(content)
         case _:
             raise ValueError('Invalid file format')
