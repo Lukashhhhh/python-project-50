b0a74013c75f5642d921e24f6c49bb06ff6a4b2f
Lukashhhhh <131288459+Lukashhhhh@users.noreply.github.com>
2024-03-26 10:03:33 +0000

    Refactoring test_generate_diff.py

--- test/test_generate_diff.py
+++ test/test_generate_diff.py
@@ -2,17 +2,22 @@ from gendiff import generate_diff
 import pytest
 
 
-@pytest.mark.parametrize("file_path1, file_path2", [
-    ('test/fixtures/file1.json', 'test/fixtures/file2.json'),
-    ('test/fixtures/file1.yml', 'test/fixtures/file2.yml'),
-    pytest.param('test/fixtures/file1.txt', 'test/fixtures/file2.txt', marks=pytest.mark.xfail)
+def get_builder_path(file_name): 
+    return 'test/fixtures/' + file_name
+
+
+@pytest.mark.parametrize("file1, file2", [
+    ('file1.json', 'file2.json'),
+    ('file1.yml', 'file2.yml'),
+    pytest.param('file1.txt', 'file2.txt', marks=pytest.mark.xfail)
 ])
-@pytest.mark.parametrize("format, result_path", [
-    ('stylish', 'test/fixtures/result_file_stylish.txt'),
-    ('plain', 'test/fixtures/result_file_plain.txt'),
-    ('json', 'test/fixtures/result_file_json.txt'),
-    pytest.param('txt', 'test/fixtures/result_file_stylish.txt', marks=pytest.mark.xfail)
+@pytest.mark.parametrize("format, result", [
+    ('stylish', 'result_file_stylish.txt'),
+    ('plain', 'result_file_plain.txt'),
+    ('json', 'result_file_json.txt'),
+    pytest.param('txt', 'result_file_stylish.txt', marks=pytest.mark.xfail)
 ])
-def test_generate_diff_stylish_plain(file_path1, file_path2, format, result_path):
+def test_generate_diff_stylish_plain(file1, file2, format, result):
+    file_path1, file_path2, result_path = map(get_builder_path, (file1, file2, result))
     with open(result_path) as result:
         assert generate_diff(file_path1, file_path2, format) == result.read()
